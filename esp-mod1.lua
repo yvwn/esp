b536669b10fffbaaf70d0304bfa10e57d9f2e1ea
yawn <68041302+yvwn@users.noreply.github.com>
2022-05-07 12:56:22 -0600

    Update esp-mod1.lua

--- esp-mod1.lua
+++ esp-mod1.lua
@@ -14,7 +14,7 @@ local ESP = {
     TeamColor = false,
     Thickness = 2,
     AttachShift = 1,
-    TeamMates = true,
+    TeamMates = false,
     Players = true,
     
     Objects = setmetatable({}, {__mode="kv"}),
